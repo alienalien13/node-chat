3ac2a647ddb52487c74bb549eccf0ad313a86b06
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-03 20:34:34 +0200

    f c

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+	app = express(),
+	logger = require('log4js').getLogger(),
+	server = require('http').Server(app),
+	io = require('socket.io')(server),
+	port = 3000;
+	
+
+server.listen(port, '127.0.0.1', function(){
+	var addr = server.address();
+	logger.level = 'debug';
+    logger.debug('listening on '+addr.address+':' + addr.port);
+}),
+
+app.use(express.static(__dirname + '/page'))
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname+'/page/index.html');
+});
+
+app.get('/link', function(req,res){
+    res.sendFile(__dirname+'/link.html');
+});
+/* app.get('/', function(req, res){
+	res.sendFile(__dirname + '/link.html')
+})
+
+app.get('/:file', function(req, res){
+
+	var options = {
+		root: __dirname + '/page/',
+		dotfiles: 'allow',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	}
+
+	res.sendFile(req.params.file, options)
+
+
+
+}) */
