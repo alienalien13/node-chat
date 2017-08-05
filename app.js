c7089d5a1d44f94139aa38d3da0ffccb370d95a2
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-05 16:14:07 +0200

    pug set, style set

--- app.js
+++ app.js
@@ -3,41 +3,33 @@ var express = require('express'),
 	logger = require('log4js').getLogger(),
 	server = require('http').Server(app),
 	io = require('socket.io')(server),
-	port = 3000;
+	port = 4000;
 
-app.use(express.static(__dirname + '/page'));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+const amazingStuff = 'Its amazing stuff, '
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname+'/page/index.html');
+    res.render('pages/index', {
+		title: 'main page',
+		message: amazingStuff + 'bro',
+		showChat: false
+	});
 });
 
-app.get('/link', function(req,res){
-    res.sendFile(__dirname+'/link.html');
+app.get('/:username', function(req,res){
+		res.render('pages/index', {
+			title: 'Hello, ' + req.params.username,
+			message: amazingStuff + req.params.username,
+			showChat: true
+		});
 });
 
 server.listen(port, '127.0.0.1', function(){
 	var addr = server.address();
 	logger.level = 'debug';
-    logger.debug('listening on '+addr.address+':' + addr.port);
+    logger.debug('listening on ' + addr.address + ':' + addr.port);
 });
-
-/* app.get('/', function(req, res){
-	res.sendFile(__dirname + '/link.html')
-})
-
-app.get('/:file', function(req, res){
-
-	var options = {
-		root: __dirname + '/page/',
-		dotfiles: 'allow',
-		headers: {
-			'x-timestamp': Date.now(),
-			'x-sent': true
-		}
-	}
-
-	res.sendFile(req.params.file, options)
-
-
-
-}) */
