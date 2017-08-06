18b6e7e4a573ebec3c83afb0ca2753cd82d3b1cd
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-06 19:44:38 +0200

    io issue

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/socket.io.js', (req,res)=>{
     res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
 });
+app.get('/socket.io/socket.io.js', (req,res)=>{
+    res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
 app.get('/jquery.js', (req,res)=>{
     res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');
 });
