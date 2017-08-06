90bde84db9a7bd57c56587f3672df349bbd93b81
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-06 20:22:34 +0200

    this. before socket

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ $('#joinChat').on('click', ()=>{
 	var userName = $('#nameInp').val()
 
 	if (userName.length > 0 && userName.indexOf(' ') == -1){
-		socket.emit('newUserJoin', userName)
+		this.socket.emit('newUserJoin', userName)
 		$('#chat').attr('class', 'container chat');
 		$('#helloPage').attr('class', 'no-content');
 		//location.href = socket.io.uri + '/' + userName
@@ -16,22 +16,22 @@ $('#joinChat').on('click', ()=>{
 })
 
 //new user join - say it to all users
-socket.on('newUser', (userData)=>{
+this.socket.on('newUser', (userData)=>{
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + 'Here is a new user: ' + userData.userName + '</div></div>')
 })
 
 //welcome to new user
-socket.on('user', (userData)=>{
+this.socket.on('user', (userData)=>{
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + 'Sup ' + userData.userName + '! U are welcome!')
 })
 
 //manage message handler
 $("input[value='send']").on('click', ()=>{
-	socket.emit('newMessage', $('textarea').val())
+	this.socket.emit('newMessage', $('textarea').val())
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 col-xs-offset-8 single-message new-user-notice">' + $('textarea').val())
 })
 
 //show new message to all
-socket.on('shareMessage', (message)=>{
+this.socket.on('shareMessage', (message)=>{
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + message)
 })
