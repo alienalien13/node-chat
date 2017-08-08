6751d6c393ce1f9210a7a908ad66811d12496e91
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-08 20:29:37 +0200

    'new'

--- public/bundle.js
+++ public/bundle.js
@@ -6422,7 +6422,7 @@ var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
 }); */
 var io = require('socket.io-client');
 var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
-	transports: ['websocket', 'polling']
+	transports: ['websocket']
 });
 /* var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
 	//path: '/socket.io-client',
