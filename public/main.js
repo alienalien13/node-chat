3970b1ebd13ce5916960f469a9ef5f6d9c6198db
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-08 20:17:20 +0200

    'new'

--- public/main.js
+++ public/main.js
@@ -4,8 +4,12 @@ var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
 	'polling duration': 10,
 	upgrade: false
 }); */
-var io;
-var socket;
+var io = require('socket.io-client');
+var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
+	transports: ['xhr-polling'],
+	'polling duration': 10,
+	upgrade: false
+});
 /* var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
 	//path: '/socket.io-client',
 	transports: ['websocket'],
@@ -20,52 +24,28 @@ var socket;
 /* socket.on('reconnect_attempt', () => {
 	socket.io.opts.transports = ['polling', 'websocket'];
 }); */
-//console.log(socket);
+console.log(socket);
 
 //new user login handler
 $('#joinChat').on('click', ()=>{
-
-	var userName = $('#nameInp').val()
-
 	 io = require('socket.io-client');
 	 socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000/', {
-		path: '/' + userName,
 		transports: ['xhr-polling'],
+		'polling duration': 10,
 		upgrade: false
 	});
-	console.log(socket);
+	var userName = $('#nameInp').val()
+
 	if (userName.length > 0 && userName.indexOf(' ') == -1){
 		socket.emit('newUserJoin', userName)
 		$('#chat').attr('class', 'container chat');
 		$('#helloPage').attr('class', 'no-content');
 		//location.href = socket.io.uri + '/' + userName
-	} else console.log('No spaces plz');
+	} else console.log('No spaces plz')
 	
-	//new user join - say it to all users
-	socket.on('newUser', (userData)=>{
-		$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + 'Here is a new user: ' + userData.userName + '</div></div>')
-	})
-
-	//welcome to new user
-	socket.on('user', (userData)=>{
-		$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + 'Sup ' + userData.userName + '! U are welcome!')
-	})
-
-	//manage message handler
-	$("input[value='send']").on('click', ()=>{
-		socket.emit('newMessage', $('textarea').val())
-		$('.messages-box').append('<div class="row"><div class="col-xs-4 col-xs-offset-8 single-message new-user-notice">' + $('textarea').val())
-	})
-
-	//show new message to all
-	socket.on('shareMessage', (message)=>{
-		$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + message)
-	})
-
-
 })
 
-/* //new user join - say it to all users
+//new user join - say it to all users
 socket.on('newUser', (userData)=>{
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + 'Here is a new user: ' + userData.userName + '</div></div>')
 })
@@ -85,4 +65,3 @@ $("input[value='send']").on('click', ()=>{
 socket.on('shareMessage', (message)=>{
 	$('.messages-box').append('<div class="row"><div class="col-xs-4 single-message new-user-notice">' + message)
 })
- */
