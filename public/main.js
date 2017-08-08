762312b28ec310e194fa619401760199196b58c6
Dmytro Ilhanaiev <ilhanaiev.dmytro@email.cz>
2017-08-08 19:23:17 +0200

    'new'

--- public/main.js
+++ public/main.js
@@ -4,11 +4,10 @@ var socket = io.connect('https://intense-inlet-33140.herokuapp.com:5000', {
 	transports: ['websocket'],
 	upgrade: false
 });
-/* this.io = socket.connect('https://intense-inlet-33140.herokuapp.com:5000', {
-	'path': '/socket.io-client',
+this.io = socket.connect('/socket.io-client', {
   transports: ['websocket'],
   upgrade: false
-}); */
+});
 console.log(socket);
 
 //new user login handler
